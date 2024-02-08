98468d82cdf240132747377ef663c45345c0c2b3
Daniel Romero <danieljromero305@gmail.com>
2024-02-08 16:39:25 -0500

    Worked on DoubleNode.java

--- src/DoubleNode/DoubleNode.java
+++ src/DoubleNode/DoubleNode.java
@@ -1,80 +1,106 @@
 package DoubleNode;
 
-public class DoubleNode
-{
-   private double data;
-   private DoubleNode link;   
+public class DoubleNode {
+    private double data;
+    private DoubleNode link;
 
 
-   public DoubleNode(double initialData, DoubleNode initialLink)
-   {
-      data = initialData;
-      link = initialLink;
-   }
+    public DoubleNode(double initialData, DoubleNode initialLink) {
+        data = initialData;
+        link = initialLink;
+    }
 
-   public double getData( )   
-   {
-      return data;
-   }
-   
-   public DoubleNode getLink( )
-   {
-      return link;                                               
-   } 
+    public double getData() {
+        return data;
+    }
 
-   public void removeNodeAfter( )   
-   {
-      link = link.link;
-   }          
-   
-   
-   public void setData(double newData)   
-   {
-      data = newData;
-   }                                                               
-   
-   public void setLink(DoubleNode newLink)
-   {                    
-      link = newLink;
-   }
-   
-   public void addNodeAfter(double item)   
-   {
-      //the source code for this method is in the notes, simply type that here.
-   }          
-      
-   public static DoubleNode listCopy(DoubleNode source)
-   {
-     
-   }
-   
-   
-   public static DoubleNode[ ] listCopyWithTail(DoubleNode source)
-   {
-	   // the source code for this method is in the notes, simply type that here.
-   }
-   
-      
-   public static int listLength(DoubleNode head)
-   {
-      
-   }
-   
-   
-   public static DoubleNode[ ] listCopyPart(DoubleNode start, DoubleNode end)
-   {
-      
-   }        
-   
-      
-   public static DoubleNode listPosition(DoubleNode head, int position)
-   {
-      
-   }
+    public DoubleNode getLink() {
+        return link;
+    }
 
-   public static DoubleNode listSearch(DoubleNode head, double target)
-   {
-      
-   }
+    public void removeNodeAfter() {
+        link = link.link;
+    }
+
+
+    public void setData(double newData) {
+        data = newData;
+    }
+
+    public void setLink(DoubleNode newLink) {
+        link = newLink;
+    }
+
+    public void addNodeAfter(double item) {
+        link = new IntNode(item, link);
+    }
+
+    public static DoubleNode listCopy(DoubleNode source) {
+
+    }
+
+
+    public static DoubleNode[] listCopyWithTail(DoubleNode source) {
+        DoubleNode copyHead, copyTail;
+        DoubleNode[] answer = new DoubleNode[2];
+
+        // Handle the special case of the empty list
+        if (source == null) {
+            return answer;
+        }
+
+        // Make the first node for the newly created list
+        copyHead = new DoubleNode(source.data, null);
+        copyTail = copyHead;
+
+        // Make the rest of the nodes for the newly created list
+        while (source.link != null) {
+            source = source.link;
+            copyTail.addNodeAfter(source.data);
+            copyTail = copyTail.link;
+        }
+
+        // Return the head and tail references
+        answer[0] = copyHead;
+        answer[1] = copyTail;
+        return answer;
+    }
+
+
+    public static int listLength(DoubleNode head) {
+        int numOfNodes = 0;
+        DoubleNode cursor = head;
+        while(cursor.link != null){
+            cursor = cursor.link;
+            numOfNodes++;
+        }
+        return numOfNodes;
+    }
+
+
+    public static DoubleNode[] listCopyPart(DoubleNode start, DoubleNode end) {
+
+    }
+
+
+    public static DoubleNode listPosition(DoubleNode head, int position) {
+        DoubleNode cursor = head;
+        for(int i = 0; i < position; i++){
+            cursor = cursor.link;
+        }
+        return cursor;
+    }
+
+    public static DoubleNode listSearch(DoubleNode head, double target) {
+        DoubleNode cursor;
+
+        for (cursor = head; cursor != null; cursor = cursor.link) {
+            if (target == cursor.data) {
+                return cursor;
+            }
+        }
+
+        return null;
+    }
 
 }
