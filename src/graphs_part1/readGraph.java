0418f87bc6828424cc918dbc11982c07995ae4a8
Daniel Romero <danieljromero305@gmail.com>
2024-03-28 16:10:46 -0400

    Worked on readGraph

--- src/graphs_part1/readGraph.java
+++ src/graphs_part1/readGraph.java
@@ -1,17 +1,18 @@
 package graphs_part1;
 
 import java.io.FileNotFoundException;
-import java.util.InputMismatchException;
+import java.io.FileReader;
+import java.io.LineNumberReader;
 import java.util.Scanner;
 import java.io.File;
 
 public class readGraph {
     public static void main(String[] args) throws FileNotFoundException {
-        int[][] graph = parseGraph(new File("src/graphs_part1/graph1_mx.txt"));
+        int[][] graph = parseGraph(new File("src/graphs_part1/graph2.txt"));
 
-        for (int i = 0; i < graph[0].length; i++) {
-            for (int j : graph[i]) {
-                System.out.print(j);
+        for (int[] ints : graph) {
+            for (int j : ints) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
@@ -20,11 +21,27 @@ public class readGraph {
     public static int[][] parseGraph(File file) throws FileNotFoundException {
         Scanner scan = new Scanner(file);
         String in;
-        int[][] graph = new int[4][4];
-        for(int i = 0; scan.hasNextLine(); i++){
+
+        int h = 0;
+        int w = 0;
+
+        for (int i = 0; scan.hasNextLine(); i++) {
             in = scan.nextLine();
             String[] tempArray = in.split(",");
-            for(int j = 0; j < tempArray.length; j++){
+            w = tempArray.length;
+            h++;
+        }
+
+        //System.out.println(w + " "+ h);
+
+        scan.close();
+        scan = new Scanner(file); // resets for parsing
+
+        int[][] graph = new int[h][w];
+        for (int i = 0; scan.hasNextLine(); i++) {
+            in = scan.nextLine();
+            String[] tempArray = in.split(",");
+            for (int j = 0; j < tempArray.length; j++) {
                 graph[i][j] = Integer.parseInt(tempArray[j]);
             }
         }
